9b2a14ff5dc65ed50d3b77c4f9af2a842b4c5351
sovw <61880799+sovw@users.noreply.github.com>
2020-03-14 09:10:09 -0700

    Update dogehub.lua

--- dogehub.lua
+++ dogehub.lua
@@ -1,12 +1,12 @@
 local library = {}
 
 local screengui = Instance.new("ScreenGui",game:GetService("CoreGui"))
+local btn = Instance.new("TextButton",screengui)
 local counter = 10
-function library:CreateWindow(name)
+function library:CreateWindow(text)
 	local a = Instance.new("TextButton",screengui)
 	a.Size = UDim2.new(0, 189,0, 32)
-	a.TextColor3 = Color3.fromRGB(255, 255, 255)
-	a.Text = name
+	a.Text = "  " .. text
 	a.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 	a.BorderSizePixel = 0
 	a.TextSize = 18
@@ -14,6 +14,15 @@ function library:CreateWindow(name)
 	a.TextXAlignment = "Left"
 	a.Position = UDim2.new(0,10,0,counter)
     counter = counter + 35
+end
+
+function library:Button(Text, Callback)
+    btn.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+    btn.TextColor3 = Color3.fromRGB(255, 255, 255)
+    btn.TextSize = 18
+    btn.Size = UDim2.new(0, 188,0, 34)
+    btn.Position = UDim2.new(0.266, 0,0.195, 0)
+    btn.MouseButton1Down:Connect(Callback)
 end
 
 return library
