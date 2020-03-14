975bf367eb6ecf60c6ee2258adab8433e3edc2ef
sovw <61880799+sovw@users.noreply.github.com>
2020-03-14 11:52:12 -0700

    Update crystalhub.lua

--- crystalhub.lua
+++ crystalhub.lua
@@ -1,17 +1,21 @@
 local library = {}
 
-local itemcount = 32
 local pos = 10
 local ui = Instance.new("ScreenGui",game:GetService("CoreGui"))
-local currentui = Instance
 
+flocal screengui = Instance.new("ScreenGui",game:GetService("CoreGui"))
+local counter = 10
 function library:CreateWindow(text)
-   local a = Instance.new("TextButton",ui)
-   a.Size = UDim2.new(0, 189,0, 32)
-   a.TextColor3 = Color3.fromRGB(255,255,255)
-   a.Text = "  " .. text
-   a.Position = UDim2.new(0,pos,0,10)
-   pos = pos + 205
+    local a = Instance.new("TextButton",ui)
+    a.Size = UDim2.new(0, 189,0, 32)
+    a.Text = "  " .. text
+    a.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+    a.BorderSizePixel = 0
+    a.TextSize = 18
+    a.Font = Enum.Font.SourceSans
+    a.TextXAlignment = "Left"
+    a.Position = UDim2.new(0,pos,0,10)
+    pos = pos + 205
 end
 
 return library
