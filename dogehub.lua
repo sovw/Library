03500e4fb45210840b9c33c22446962ab505efed
sovw <61880799+sovw@users.noreply.github.com>
2020-03-14 04:25:23 -0700

    Update dogehub.lua

--- dogehub.lua
+++ dogehub.lua
@@ -1,15 +1,18 @@
 local library = {}
 
 local screengui = Instance.new("ScreenGui",game:GetService("CoreGui"))
-function library:CreateWindow(text)
+local counter = 10
+function library:CreateWindow(name)
 	local a = Instance.new("TextButton",screengui)
-	a.Position = UDim2(0.018, 0,0.026, 0)
-	a.Text = text;
+	a.Size = UDim2.new(0, 189,0, 32)
+	a.Text = name
 	a.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
 	a.BorderSizePixel = 0
 	a.TextSize = 18
 	a.Font = Enum.Font.SourceSans
 	a.TextXAlignment = "Left"
+	a.Position = UDim2.new(0,10,0,counter)
+    counter = counter + 20
 end
 
 return library
